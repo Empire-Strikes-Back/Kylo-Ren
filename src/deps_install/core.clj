60ee4612a84eeebb380c3c4e1e19a8cde5b473fb
deps-install <deps-install@library>
2021-08-03 01:17:07 +0300

    i remove main, add core

--- src/deps_install/core.clj
+++ src/deps_install/core.clj
@@ -1,5 +1,4 @@
-(ns deps-install.main
-  (:gen-class)
+(ns deps-install.core
   (:require
    [clojure.core.async :as a :refer [chan go go-loop <! >! <!! >!!  take! put! offer! poll! alt! alts! close! onto-chan!
                                      pub sub unsub mult tap untap mix admix unmix pipe
@@ -9,24 +8,18 @@
    [clojure.spec.alpha :as s]
    [clojure.java.io :as io]))
 
-(println "clojure.compiler.direct-linking" (System/getProperty "clojure.compiler.direct-linking"))
-(clojure.spec.alpha/check-asserts true)
 (do (set! *warn-on-reflection* true) (set! *unchecked-math* true))
 
-(defonce stateA (atom nil))
+(defn process
+  [opts]
+  (let [default-opts {}
+        {:keys []} (merge default-opts opts)]))
 
-(defn -main [& args]
-  (println ::-main)
-  (let [data-dir (-> (io/file (System/getProperty "user.dir")) (.getCanonicalPath))]
-    (reset! stateA {})
-    (add-watch stateA :watch-fn (fn [k stateA old-state new-state] new-state))
-
-    (go)))
 
 (comment
 
   (require
-   '[deps-install.main]
+   '[deps-install.core]
    :reload)
 
   (-main)
