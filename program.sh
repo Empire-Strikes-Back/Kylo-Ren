d31ee2e9440545dbf2234c78cbb2efdb20e39cbc
deps-install <deps-install@program>
2021-08-03 00:35:31 +0300

    to create uberjar i use genie library

--- program.sh
+++ program.sh
@@ -10,4 +10,13 @@ repl(){
     :nrepl? false
 }
 
+uberjar(){
+  clj \
+    -X:uberjar genie.core/process \
+    :uberjar-name out/deps-install.standalone.jar \
+    :main-ns deps-install.main
+  mkdir -p out/jpackage-input
+  mv out/deps-install.standalone.jar out/jpackage-input/
+}
+
 "$@"
