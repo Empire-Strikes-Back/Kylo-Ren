cac49bae200031929408392e8b8c41766b09c6ba
deps-install <deps-install@program>
2021-08-03 00:37:27 +0300

    to program.sh i add j-package

--- program.sh
+++ program.sh
@@ -26,4 +26,31 @@ uberjar(){
   mv out/deps-install.standalone.jar out/jpackage-input/
 }
 
+j-package(){
+  OS=${1:?"Need OS type (windows/linux/mac)"}
+
+  echo "Starting build..."
+
+  if [ "$OS" == "windows" ]; then
+    J_ARG="--win-menu --win-dir-chooser --win-shortcut"
+          
+  elif [ "$OS" == "linux" ]; then
+      J_ARG="--linux-shortcut"
+  else
+      J_ARG=""
+  fi
+
+  jpackage \
+    --input out/jpackage-input \
+    --dest out \
+    --main-jar deps-install.standalone.jar \
+    --name "deps-install" \
+    --main-class clojure.main \
+    --arguments -m \
+    --arguments deps-install.main \
+    --app-version "1" \
+    $J_ARG
+  
+}
+
 "$@"
