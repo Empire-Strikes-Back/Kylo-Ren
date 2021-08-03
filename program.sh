cef2d86c5e8a9c046500148c8b609d1cd8f54e97
deps-install <deps-install@program>
2021-08-03 21:11:41 +0300

    i revert "to create uberjar i use genie library"
    This reverts commit d31ee2e9440545dbf2234c78cbb2efdb20e39cbc.

--- program.sh
+++ program.sh
@@ -10,13 +10,4 @@ repl(){
     :nrepl? false
 }
 
-uberjar(){
-  clj \
-    -X:uberjar genie.core/process \
-    :uberjar-name out/deps-install.standalone.jar \
-    :main-ns deps-install.main
-  mkdir -p out/jpackage-input
-  mv out/deps-install.standalone.jar out/jpackage-input/
-}
-
 "$@"
