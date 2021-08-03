b8ae206302ee431b1169ef2d7e0e64d068004f59
deps-install <deps-install@program>
2021-08-03 21:11:32 +0300

    i revert "to program.sh i add main"
    This reverts commit 1da06e6f075d6fd3cf9e4eb4e4d2d95c4f5872f7.

--- program.sh
+++ program.sh
@@ -10,13 +10,6 @@ repl(){
     :nrepl? false
 }
 
-main(){
-  clojure \
-    -J-Dclojure.core.async.pool-size=1 \
-    -J-Dclojure.compiler.direct-linking=false \
-    -M -m deps-install.main
-}
-
 uberjar(){
   clj \
     -X:uberjar genie.core/process \
